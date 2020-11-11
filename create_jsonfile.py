ea9d5c269c57071a662b39041071c980b026e5b6
Wagner <wagnermz@mbp314.fritz.box>
2020-11-11 20:55:28 +0100

    works now on single image

--- create_jsonfile.py
+++ create_jsonfile.py
@@ -1,54 +1,75 @@
 import os
 import json
 import pdb
+import pandas as pd
+import argparse
 
 
-def assign_img_to_json(path):
+def parse_args():
+    parser = argparse.ArgumentParser(description="create relevant json files from images")
+    parser.add_argument('--path', type=str, default="./data",
+                        help='path were user folders are stored')
+    parser.add_argument('--multi', type=bool, default=True,
+                        help='user for whom the results shall be retrieved')
+    args = parser.parse_args()
+    return args
+
+
+def assign_img_to_json(args):
     """[summary]
 
     Args:
         path ([type]): [description]
     """
+    path = args.path
+    multi = args.multi
     absolute_path = os.path.abspath(path)
     #absolute_path = path
     #absolute_path1 = absolute_path[1:]
-    path_low_energy = f"{absolute_path}/low-energy/"
-    path_high_energy = f"{absolute_path}/high-energy/"
+    if multi:
+        path_low_energy = f"{absolute_path}/low-energy/"
+        path_high_energy = f"{absolute_path}/high-energy/"
 
-    count = 0
-    for img1, img2 in zip(os.listdir(path_low_energy),
-                          os.listdir(path_high_energy)):
+        count = 0
 
-        # create dictionary with the right keys
-        # dictionary = {'image1': f"http://0.0.0.0:8080/data/filename?d={path_low_energy}{img1}",
-        #               'image2': f"http://0.0.0.0:8080/data/filename?d={path_high_energy}{img2}"}
+        for img1, img2 in zip(os.listdir(path_low_energy),
+                              os.listdir(path_high_energy)):
 
-        dictionary = {'image1': f"http://0.0.0.0:8080{path_low_energy}{img1}",
-                      'image2': f"http://0.0.0.0:8080{path_high_energy}{img2}"}
-        # dictionary = {'image1': f"file///{path_low_energy}{img1}",
-        #               'image2': f"file///{path_high_energy}{img2}"}
-        # dictionary = {'image1': f"{path_low_energy}{img1}",
-        #               'image2': f"{path_high_energy}{img2}"}
+            # create dictionary with the right keys
+            # dictionary = {'image1': f"http://0.0.0.0:8080/data/filename?d={path_low_energy}{img1}",
+            #               'image2': f"http://0.0.0.0:8080/data/filename?d={path_high_energy}{img2}"}
 
-        # save as unique json file
-        with open(f"{absolute_path}/json_files/json{count}.json", "w") as json_file:
-            json.dump(dictionary, json_file)
+            # dictionary = {'image1': f"http://0.0.0.0:8080{path_low_energy}{img1}",
+            #               'image2': f"http://0.0.0.0:8080{path_high_energy}{img2}"}
+            dictionary = {'image1': f"file://{path_low_energy}{img1}",
+                          'image2': f"file://{path_high_energy}{img2}"}
+            # dictionary = {'image1': f"{path_low_energy}{img1}",
+            #               'image2': f"{path_high_energy}{img2}"}
 
-        count += 1
+            # save as unique json file
+            with open(f"{absolute_path}/json_files/json{count}.json", "w") as json_file:
+                json.dump(dictionary, json_file)
 
-# def assign_img_to_json_alternative(path):
-#     """[summary]
+            count += 1
 
-#     Args:
-#         path ([type]): [description]
-#     """
+    else:
+        path_all = f"{absolute_path}/data2/"
+        count = 0
+        for img, in zip(os.listdir(path_all)):
+            dictionary = {'image': f"file://{path_all}{img}"}
 
-#     absolute_path = os.path.abspath(path)
+            with open(f"{absolute_path}/json_files/json{count}.json", "w") as json_file:
+                json.dump(dictionary, json_file)
+
+            count += 1
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    assign_img_to_json(args)
 
 
 
+# path = './data'
 
-
-path = './data'
-
-assign_img_to_json(path=path)
+# assign_img_to_json(path=path, multi=False)
---
+++ store_results.py
@@ -0,0 +1,42 @@
+import os
+import json
+import pdb
+import pandas as pd
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="store annotation results from respective user")
+    parser.add_argument('--path', type=str, default=".",
+                        help='path were user folders are stored')
+    parser.add_argument('--user', type=str, default='moritz',
+                        help='user for whom the results shall be retrieved')
+    args = parser.parse_args()
+    return args
+
+
+def retrieve_annotations(args):
+    """[summary]
+
+    Args:
+        path ([type]): [description]
+    """
+    path = args.path
+    user = args.user
+    absolute_path = f"{path}/{user}/ma/completions"
+
+    df = pd.DataFrame(columns=['file', 'label'])
+
+    for json_file in os.listdir(absolute_path):
+        with open(f"{absolute_path}/{json_file}", "r") as read_file:
+            data = json.load(read_file)
+            choice = data['completions'][0]['result'][0]['value']['choices']
+            instance = {'file': json_file, 'label': choice}
+            df = df.append(instance, ignore_index=True)
+
+    df.to_csv(f"./{user}_annotations.csv")
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    retrieve_annotations(args)
